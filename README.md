02781b22ae475d7501779c0037adc652fbe19868
Dimas Ciputra <dimas.ciputra@gmail.com>
2025-03-10 17:01:50 +0000

    Add django docker compose configuration (#34)

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+# SANSA Water Quality and Quantity Public
 
+A boilerplate for kickstarting Kartoza Django backend and React frontend projects
+
+
+## ⚡ Quick Start
+
+- Clone the repository
+```
+git clone https://github.com/kartoza/SANSA-Water-Quality-and-Quantity.git
+cd SANSA-Water-Quality-and-Quantity
+```
+
+- Copy and customize the environment file and the Docker Compose override file from the template.
+```
+cp deployment/.template.env deployment/.env
+cp deployment/docker-compose.override.template.yml deployment/docker-compose.override.yml
+```
+
+- Build the project using the provided command in makefile
+```
+make build
+```
+
+- Run the application using Docker and the provided makefile
+```
+make run
+```
+
+## Developer Guide
+
+https://kartoza.github.io/django-react-base/developer/guide/
+
+## 📚 Documentation
+For detailed setup instructions, custom configurations, deployment, and additional features, please refer to the wiki.
+
+## 🤝 Contributing
+Contributions are welcome! Please read our contributing guide to learn how you can get involved and help improve this project.
+
+## 📄 License
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ django_project/core/settings/contrib.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""Settings for 3rd party."""
+from .base import *  # noqa
+
+# Extra installed apps
+INSTALLED_APPS = INSTALLED_APPS + (
+    'rest_framework',
+    'rest_framework_gis',
+    'guardian',
+    'django_cleanup.apps.CleanupConfig',
+    'django_celery_beat',
+    'django_celery_results',
+)
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_VERSIONING_CLASS': (
+        'rest_framework.versioning.NamespaceVersioning'
+    ),
+}
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',  # default
+    'guardian.backends.ObjectPermissionBackend',
+)
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+CELERY_RESULT_BACKEND = 'django-db'
+
+TEMPLATES[0]['OPTIONS']['context_processors'] += [
+    'django.template.context_processors.request',
+]
+
+SENTRY_DSN = os.environ.get('SENTRY_DSN', '')
