3a60c77e9a2441a6fa40a4dc91606103bda03339
zamuzakki <zakki@kartoza.com>
2025-03-13 06:49:09 +0000

    Setup monthly task

---
+++ django_project/project/tasks.py
@@ -0,0 +1,11 @@
+from celery.utils.log import get_task_logger
+from celery import shared_task
+from core.celery import app
+
+logger = get_task_logger(__name__)
+
+
+@app.task(name='update_stored_data')
+def update_stored_data():
+    """Update stored data."""
+    logger.info('Updating stored data')
