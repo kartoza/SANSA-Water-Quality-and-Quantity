4a65d3e0a61485ccaf9f829b6102431384f13aaa
Jeff Osundwa <jeff@kartoza.com>
2025-03-18 04:46:11 +0300

    API Development for Water Surface Area Extent and Altimeter Fixes #11

--- django_project/project/urls.py
+++ django_project/project/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from project.api_views.analysis import WaterAnalysisAPIView, AnalysisTaskStatusAPIView
 from project.views.dataset import DatasetOverviewView
+from project.views import AWEIWaterExtentView, AWEIWaterMaskView
 
 urlpatterns = [
     path('datasets/', DatasetOverviewView.as_view(), name="datasets-overview"),
     path('water-analysis/', WaterAnalysisAPIView.as_view(), name="water-analysis"),
-    path('analysis-task/<uuid:task_uuid>/', AnalysisTaskStatusAPIView.as_view(), name='analysis-task-status')
+    path('analysis-task/<uuid:task_uuid>/', AnalysisTaskStatusAPIView.as_view(), name='analysis-task-status'),
+    path(
+        "awei-water-extent/",
+        AWEIWaterExtentView.as_view(),
+        name="awei-water-extent"
+    ),
+    path(
+        "awei-water-mask/",
+        AWEIWaterMaskView.as_view(),
+        name="awei-water-mask"
+    ),
 ]
